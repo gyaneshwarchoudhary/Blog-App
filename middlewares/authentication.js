eba4e13040ad821102fc3122ddce6967fc05bae1
gyaneshwarchoudhary <gyaneshwarchoudhary637@gmail.com>
2024-03-02 19:17:14 +0530

    made changes

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -4,14 +4,14 @@ function checkForAuthentificationCookie(cookieName) {
   return (req, res, next) => {
     const TokenCookieValue = req.cookies[cookieName];
     if (!TokenCookieValue) {
-      next();
+      return next();
     }
 
     try {
       const userPayload = validateToken(TokenCookieValue);
       req.user = userPayload;
     } catch (error) {}
-    next();
+    return next();
   };
 }
 
